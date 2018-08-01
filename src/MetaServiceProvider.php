6a8f80d87182032ca59b8c2e72f263419f04a619
MrLikviduk <v@086.kz>
2018-08-01 17:42:35 +0600

    Initial commit 2

--- src/MetaServiceProvider.php
+++ src/MetaServiceProvider.php
@@ -2,13 +2,19 @@
 
 namespace Kalinkin\Meta;
 
+use App\Jobs\SendRegistrationMail;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class MetaServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        Event::listen(Registered::class, function ($event) {
+            Queue::push(new SendRegistrationMail($event->user));
+        });
     }
 
     public function register()
